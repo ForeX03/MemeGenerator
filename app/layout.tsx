5d7093aa74f9d767345059703793db70c7837094
ForeX03 <kofdextv@gmail.com>
2025-05-28 19:21:54 +0200

    added author paragraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="en" className="w-full h-full">
       <body className="relative antialiased w-full h-full">
         { children }
-        <div className="absolute z-50 bottom-4 left-4">ForeX03</div>
+        <div className="absolute z-50 text-gray-300 bottom-2 left-2">Created by ForeX03</div>
       </body>
     </html>
   );
