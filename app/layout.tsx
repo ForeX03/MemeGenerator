15e983e40d2fcf31475723cb06d7b0a5b5ab606c
ForeX03 <kofdextv@gmail.com>
2025-05-28 19:20:54 +0200

    added author paragraph

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="w-full h-full">
-      <body className="antialiased w-full h-full">
+      <body className="relative antialiased w-full h-full">
         { children }
         <div className="absolute z-50 bottom-4 left-4">ForeX03</div>
       </body>
